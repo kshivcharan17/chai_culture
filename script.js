7413918e463b0c4cf21cfe14bc1b83de125bf9ab
Shivcharan <146615146+kshivcharan17@users.noreply.github.com>
2026-01-28 19:28:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.querySelector("button").addEventListener("click", () => {
     alert("Thank you! We will notify you at launch.");
 });
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".container").style.opacity = "0";
+    setTimeout(() => {
+        document.querySelector(".container").style.transition = "opacity 1.2s ease";
+        document.querySelector(".container").style.opacity = "1";
+    }, 200);
+});
